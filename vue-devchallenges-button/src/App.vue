44304158e714f626aa760c34a1c9549db14f6edb
Aldrin Esteban <aresteban1@gmail.com>
2021-01-23 22:16:37 +0800

    Add new stuff

--- vue-devchallenges-button/src/App.vue
+++ vue-devchallenges-button/src/App.vue
@@ -1,35 +1,187 @@
 <template>
-  <br>
-  <br>
-  <Button value="Button" disabled/> <span> -- </span>
-  <Button value="Button" color="primary"/> <span> -- </span>
-  <Button value="Button" color="secondary"/><span> -- </span>
-  <Button value="Button" color="danger" class="potato" data-test="s" disableShadow/>
-  <br>
-  <br>
-  <Button value="Button" variant="outline"/>
+    <div class="container">
+        <section class="section-buttons">
+            <h1>Variant: Default</h1>
+            <hr />
+            <div class="layer">
+                <div class="button-wrapper">
+                    <span> Default </span>
+                    <Button value="Button" />
+                </div>
 
-  <br>
-  <br>
-  <Button value="Button" variant="text" endIcon="alarm"/>
+                <div class="button-wrapper">
+                    <span> Disabled </span>
+                    <Button value="Button" disabled />
+                </div>
 
-  <button disabled> Test </button>
+                <div class="button-wrapper">
+                    <span>Icon Enabled</span>
+                    <Button value="Button" startIcon="add_shopping_cart" />
+                </div>
+            </div>
+            <div class="layer">
+                <div class="button-wrapper">
+                    <span>Default</span>
+                    <Button value="Button" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span>Primary</span>
+                    <Button value="Button" color="primary" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span>Secondary</span>
+                    <Button value="Button" color="secondary" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span>Danger</span>
+                    <Button value="Button" color="danger" />
+                </div>
+            </div>
+        </section>
+
+        <section class="section-buttons">
+            <h1>Variant: Outline</h1>
+            <hr />
+            <div class="layer">
+                <div class="button-wrapper">
+                    <span> Default </span>
+                    <Button value="Button" variant="outline" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Disabled </span>
+                    <Button value="Button" variant="outline" disabled />
+                </div>
+
+                <div class="button-wrapper">
+                    <span>Icon Enabled</span>
+                    <Button
+                        value="Button"
+                        startIcon="add_shopping_cart"
+                        variant="outline"
+                    />
+                </div>
+            </div>
+            <div class="layer">
+                <div class="button-wrapper">
+                    <span> Default </span>
+                    <Button value="Button" variant="outline" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Primary </span>
+                    <Button value="Button" variant="outline" color="primary" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Secondary </span>
+                    <Button
+                        value="Button"
+                        variant="outline"
+                        color="secondary"
+                    />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Danger </span>
+                    <Button value="Button" variant="outline" color="danger" />
+                </div>
+            </div>
+        </section>
+
+        <section class="section-buttons">
+            <h1>Variant: Text</h1>
+            <hr />
+            <div class="layer">
+                <div class="button-wrapper">
+                    <span> Default </span>
+                    <Button value="Button" variant="text" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Disabled </span>
+                    <Button value="Button" variant="text" disabled />
+                </div>
+
+                <div class="button-wrapper">
+                    <span>Icon Enabled</span>
+                    <Button
+                        value="Button"
+                        startIcon="add_shopping_cart"
+                        variant="text"
+                    />
+                </div>
+            </div>
+            <div class="layer">
+                <div class="button-wrapper">
+                    <span> Default </span>
+                    <Button value="Button" variant="text" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Primary </span>
+                    <Button value="Button" variant="text" color="primary" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Secondary </span>
+                    <Button value="Button" variant="text" color="secondary" />
+                </div>
+
+                <div class="button-wrapper">
+                    <span> Danger </span>
+                    <Button value="Button" variant="text" color="danger" />
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import Button from './components/Button.vue'
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Button
-  }
-}
+    name: "App",
+    components: {
+        Button,
+    },
+};
 </script>
 
-<style>
-.potato {
-  color: white;
-  background: burlywood !important;
+<style lang="scss">
+.container {
+    padding-top: 20px;
+    display: flex;
+    justify-content: space-around;
+    
+}
+
+.section-buttons {
+    box-shadow: 0px 8px 10px rgba(51, 51, 51, 0.2);
+    border-radius: 10px;
+    padding: 20px 15px;
+    font-family: "Noto Sans JP", sans-serif;
+    margin-bottom: 20px;
+
+    h1 {
+        margin-bottom: 0.5em;
+    }
+
+    .layer {
+        display: flex;
+    }
+
+    .button-wrapper {
+        span {
+            margin-bottom: 5px;
+        }
+
+        display: flex;
+        flex-direction: column;
+        margin: 5px 10px;
+    }
 }
 </style>
--- vue-devchallenges-button/src/components/Button.vue
+++ vue-devchallenges-button/src/components/Button.vue
@@ -1,160 +1,232 @@
 <template>
-    <button :class="{
-        'variant-default': !variant && 'default', 
-        'variant-outline': variant == 'outline', 
-        'variant-text': variant == 'text',
-        'shadow-disabled': disableShadow,
-        'shadow-default': !disableShadow,
-        ['size-' + size] : size,
-        ['color-'+ color]: color,
-        'disabled': disabled }">
-
-            <i v-if="startIcon" class="material-icons">{{ startIcon }}</i>
-            {{ value }}
-            <i v-if="endIcon" class="material-icons">{{ endIcon }}</i>
-        <!-- <div class="button-content">
-        </div> -->
+    <button
+        :class="{
+            'variant-default': !variant && 'default',
+            'variant-outline': variant == 'outline',
+            'variant-text': variant == 'text',
+            'shadow-disabled': disableShadow,
+            'shadow-default': !disableShadow,
+            ['size-' + size]: size,
+            ['color-' + color]: color,
+            disabled: disabled,
+        }"
+    >
+        <i v-if="startIcon" class="material-icons">{{ startIcon }}</i>
+        {{ value }}
+        <i v-if="endIcon" class="material-icons">{{ endIcon }}</i>
     </button>
 </template>
 
 <script>
 export default {
-    // This makes basic attributes modifiable from the parent. 
+    // This makes basic attributes modifiable from the parent.
     inheritAttrs: true,
     // Alternatively, I can add a v-bind="$attrs" if target element is not the root element.
 
     props: {
-        value: String, 
-        variant: Number, 
+        value: String,
+        variant: Number,
         disabled: Boolean,
         disableShadow: Boolean,
         startIcon: String,
         endIcon: String,
         size: String,
-        color: String
+        color: String,
     },
 
     data() {
-        return {
-            
-        }
+        return {};
     },
-
-
-}
+};
 </script>
 
 <style lang="scss" scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+// @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
 
-    $background-gray: #E0E0E0;
-    $background-blue: #3D5AFE;
-    $background-blue-2: #2962ff1a;
-    $font: 'Noto Sans', sans-serif;
+$background-gray: #e0e0e0;
+$background-blue: #3d5afe;
+$background-blue-2: #2962ff1a;
+$font: "Noto Sans", sans-serif;
 
-    button { 
-        cursor: pointer; 
-        padding: 8px 20px;
-        border-radius: 6px;
-        border: none;
-        font-family: $font;
-        font-size: 14px; 
+button {
+    cursor: pointer;
+    padding: 8px 20px;
+    border-radius: 6px;
+    border: none;
+    font-family: $font;
+    font-size: 14px;
 
-        &:hover, &:focus {
-            transform: translateY(-3px);
-        }
-
-        transition: all 150ms ease-out 0s;
-        transform: translateY(0px);
+    &:hover,
+    &:focus {
+        transform: translateY(-3px);
     }
 
-    // default 24px is just way too big for 14px text
-    i { 
-        font-size: 18px; 
-        vertical-align: middle;
-    } 
+    transition: all 150ms ease-out 0s;
+    transform: translateY(0px);
+}
 
-    .variant-default {
-        background: $background-gray;
+// default 24px is just way too big for 14px text
+i {
+    font-size: 18px;
+    vertical-align: middle;
+}
 
-        &:hover, &:focus {
-            background: darken($background-gray, 20);
-            box-shadow: 0px 4px 6px rgba(51, 51, 51, 0.2);
-        }
-    }
+.variant-default {
+    background: $background-gray;
 
-    .variant-outline {
-        border: 1px solid #3D5AFE;
-        border-radius: 6px;
-        background: none;
-        color: #3D5AFE;
-        
-        &:hover, &:focus { background: $background-blue-2; }
-    }
-
-    .variant-text {
-        background: none;
-        color: #3D5AFE;
+    &.disabled {
         box-shadow: none;
-
-        &:hover, &:focus { background: $background-blue-2; }
+        background: darken($background-gray, 20);
+        color: darken($background-gray, 40);
     }
 
-    .hoverable {
-        &:hover, &:focus {
-            // background: darken($background-gray, 20);
-            transform: translate(0px, -3px);
-
-        }
-    }
-
-    .shadow-disabled { box-shadow: none !important; }
-    .shadow-default { 
-        box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2); 
-        
-        &:hover, &:focus { box-shadow: 0px 5px 7px rgba(51, 51, 51, 0.2) }
-    }
-    
-    .size-sm { padding: 6px 12px; }
-    .size-md { padding: 8px 20px; }
-    .size-lg { padding: 11px 22px; }
-
-    // .color-default { }
-    .color-primary { 
+    &.color-primary {
         color: #fff;
-        background: #2962FF;
+        background: #2962ff;
 
-        &:hover, &:focus {
-            background: darken(#2962FF, 18);
+        &:hover,
+        &:focus {
+            background: darken(#2962ff, 18);
         }
     }
 
-    .color-secondary { 
+    &.color-secondary {
         color: #fff;
-        background: #455A64;
+        background: #455a64;
 
-        &:hover, &:focus {
-            background: darken(#455A64, 18);
-        }
-    }
-    .color-danger {
-        color: #fff; 
-        background: #D32F2F;
-
-        &:hover, &:focus {
-            background: darken(#D32F2F, 18);
+        &:hover,
+        &:focus {
+            background: darken(#455a64, 18);
         }
     }
 
-    .disabled {
-        cursor: not-allowed;
-        color: lighten(#000, 20);
-        background: lighten($background-gray, 20);
+    &.color-danger {
+        color: #fff;
+        background: #d32f2f;
 
-        &:hover, &:focus {
-            transform: translateY(0px);
-            box-shadow: none;
+        &:hover,
+        &:focus {
+            background: darken(#d32f2f, 18);
         }
     }
-    // Remove hovered and pointer when disabled
+
+    &:hover,
+    &:focus {
+        background: darken($background-gray, 20);
+        box-shadow: 0px 4px 6px rgba(51, 51, 51, 0.2);
+    }
+}
+
+.variant-outline {
+    border: 1px solid #3d5afe;
+    border-radius: 6px;
+    background: none;
+    color: #3d5afe;
+
+    &.disabled {
+        box-shadow: none;
+        background: darken($background-blue-2, 20);
+        color: lighten(#3d5afe, 10);
+    }
+
+    &.color-primary {
+        border-color: #2962ff;
+        color: #2962ff;
+    }
+
+    &.color-secondary {
+        border-color: #455a64;
+        color: #455a64;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#455a64, 60);
+        }
+    }
+
+    &.color-danger {
+        border-color: #d32f2f;
+        color: #d32f2f;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#d32f2f, 40);
+        }
+    }
+
+    &:hover,
+    &:focus {
+        background: $background-blue-2;
+    }
+}
+
+.variant-text {
+    background: none;
+    color: #3d5afe;
+    box-shadow: none;
+
+    &.disabled {
+        color: lighten(#3d5afe, 20);
+        background: $background-blue-2;
+    }
+
+    &.color-secondary {
+        color: #455a64;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#455a64, 60);
+        }
+    }
+
+    &.color-danger {
+        color: #d32f2f;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#d32f2f, 40);
+        }
+    }
+
+    &.shadow-default {
+        box-shadow: none;
+    }
+
+    &:hover,
+    &:focus {
+        background: $background-blue-2;
+    }
+}
+
+.shadow-disabled {
+    box-shadow: none !important;
+}
+.shadow-default {
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+
+    &:hover,
+    &:focus {
+        box-shadow: 0px 5px 7px rgba(51, 51, 51, 0.2);
+    }
+}
+
+.size-sm {
+    padding: 6px 12px;
+}
+.size-md {
+    padding: 8px 20px;
+}
+.size-lg {
+    padding: 11px 22px;
+}
+
+.disabled {
+    cursor: not-allowed;
+
+    &:hover,
+    &:focus {
+        transform: translateY(0px);
+        box-shadow: none;
+    }
+}
 </style>
