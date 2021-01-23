9e9b6628f1866fb478b470022cb052f4c50f9513
aesteban4 <aesteban4@dxc.com>
2021-01-23 16:56:46 +0800

    Prototype solution for buttons module

--- vue-devchallenges-button/src/App.vue
+++ vue-devchallenges-button/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <br>
+  <br>
+  <Button value="Button" disabled/> <span> -- </span>
+  <Button value="Button" color="primary"/> <span> -- </span>
+  <Button value="Button" color="secondary"/><span> -- </span>
+  <Button value="Button" color="danger" class="potato" data-test="s" disableShadow/>
+  <br>
+  <br>
+  <Button value="Button" variant="outline"/>
+
+  <br>
+  <br>
+  <Button value="Button" variant="text" endIcon="alarm"/>
+
+  <button disabled> Test </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.potato {
+  color: white;
+  background: burlywood !important;
 }
 </style>
