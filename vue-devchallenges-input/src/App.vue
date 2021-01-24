468a1f5968b1ba4b310684e3cc59ad0386f2131b
Aldrin Esteban <aresteban1@gmail.com>
2021-01-24 23:45:21 +0800

    Done module for input field

--- vue-devchallenges-input/src/App.vue
+++ vue-devchallenges-input/src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="input-container">
+        <div>
+            <Input id="item1" label="Label" />
+        </div>
+        <br>
+        <div>
+            <Input id="item2" label="Label" error />
+        </div>
+        <br>
+        <div>
+            <Input id="item3" label="Label" disabled />
+        </div>
+        <br>
+        <div>
+            <Input id="item4" label="Label" helperText="I am helping!" class="mr-2"/>
+            <Input id="item5" label="Label" helperText="I am helping!" error />
+        </div>
+        <br>
+        <div>
+            <Input id="item6" label="Label" size="sm" helperText="Look Im helping!" startIcon="add_shopping_cart" class="mr-2"/>
+            <Input id="item7" label="Label" endIcon="add_shopping_cart" />
+        </div>
+        <br>
+        <div>
+            <Input id="item8" label="Label"  value="Test Value" class="mr-2"/>
+        </div>
+
+        <br>
+        <div>
+            <Input id="item9" label="Label"  size="sm" class="mr-2"/>
+            <Input id="item10" label="Label"  size="md" class="mr-2"/>
+        </div>
+        <br>
+        <div>
+            <Input id="item11" label="Label"  size="sm" fullWidth/>
+        </div>
+        <br>
+        <div>
+            <Input id="item11" label="Label"  rows="6"/>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: { Input },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* { font-family: 'Noto Sans JP', sans-serif; }
+
+.input-container {
+    padding: 20px 30px;
+}
+
+.mr-2 {
+    margin-right: 6px;
 }
 </style>
