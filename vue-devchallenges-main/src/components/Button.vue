2318d5827b4fd2cbaf3b4c30d40bb419e3852219
aesteban4 <aesteban4@dxc.com>
2021-02-05 00:50:19 +0800

    Copied previously created components to here. See description.
    Could not figure out how to include components outside main directory to postcss config. Looks like issue with building the project. Relearn cofiguring webpack or babel to solve this issue.

---
+++ vue-devchallenges-main/src/components/Button.vue
@@ -0,0 +1,233 @@
+<template>
+    <button
+        :class="{
+            'variant-default': !variant && 'default',
+            'variant-outline': variant == 'outline',
+            'variant-text': variant == 'text',
+            'shadow-disabled': disableShadow,
+            'shadow-default': !disableShadow,
+            ['size-' + size]: size,
+            ['color-' + color]: color,
+            disabled: disabled,
+        }"
+    >
+        <i v-if="startIcon" class="material-icons">{{ startIcon }}</i>
+        {{ value }}
+        <i v-if="endIcon" class="material-icons">{{ endIcon }}</i>
+    </button>
+</template>
+
+<script>
+export default {
+    // This makes basic attributes modifiable from the parent.
+    inheritAttrs: true,
+    // Alternatively, I can add a v-bind="$attrs" if target element is not the root element.
+
+    props: {
+        value: {type: String, default: "Button"},
+        variant: Number,
+        disabled: Boolean,
+        disableShadow: Boolean,
+        startIcon: String,
+        endIcon: String,
+        size: String,
+        color: String,
+    },
+
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+// @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+
+$background-gray: #e0e0e0;
+$background-blue: #3d5afe;
+$background-blue-2: #2962ff1a;
+$font: "Noto Sans", sans-serif;
+
+button {
+    cursor: pointer;
+    padding: 8px 20px;
+    border-radius: 6px;
+    border: none;
+    font-family: $font;
+    font-size: 14px;
+    outline: 0;
+
+    &:hover,
+    &:focus {
+        transform: translateY(-3px);
+    }
+
+    transition: all 150ms ease-out 0s;
+    transform: translateY(0px);
+}
+
+// default 24px is just way too big for 14px text
+i {
+    font-size: 18px;
+    vertical-align: middle;
+}
+
+.variant-default {
+    background: $background-gray;
+
+    &.disabled {
+        box-shadow: none;
+        background: darken($background-gray, 20);
+        color: darken($background-gray, 40);
+    }
+
+    &.color-primary {
+        color: #fff;
+        background: #2962ff;
+
+        &:hover,
+        &:focus {
+            background: darken(#2962ff, 18);
+        }
+    }
+
+    &.color-secondary {
+        color: #fff;
+        background: #455a64;
+
+        &:hover,
+        &:focus {
+            background: darken(#455a64, 18);
+        }
+    }
+
+    &.color-danger {
+        color: #fff;
+        background: #d32f2f;
+
+        &:hover,
+        &:focus {
+            background: darken(#d32f2f, 18);
+        }
+    }
+
+    &:hover,
+    &:focus {
+        background: darken($background-gray, 20);
+        box-shadow: 0px 4px 6px rgba(51, 51, 51, 0.2);
+    }
+}
+
+.variant-outline {
+    border: 1px solid #3d5afe;
+    border-radius: 6px;
+    background: none;
+    color: #3d5afe;
+
+    &.disabled {
+        box-shadow: none;
+        background: darken($background-blue-2, 20);
+        color: lighten(#3d5afe, 10);
+    }
+
+    &.color-primary {
+        border-color: #2962ff;
+        color: #2962ff;
+    }
+
+    &.color-secondary {
+        border-color: #455a64;
+        color: #455a64;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#455a64, 60);
+        }
+    }
+
+    &.color-danger {
+        border-color: #d32f2f;
+        color: #d32f2f;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#d32f2f, 40);
+        }
+    }
+
+    &:hover,
+    &:focus {
+        background: $background-blue-2;
+    }
+}
+
+.variant-text {
+    background: none;
+    color: #3d5afe;
+    box-shadow: none;
+
+    &.disabled {
+        color: lighten(#3d5afe, 20);
+        background: $background-blue-2;
+    }
+
+    &.color-secondary {
+        color: #455a64;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#455a64, 60);
+        }
+    }
+
+    &.color-danger {
+        color: #d32f2f;
+
+        &:hover,
+        &:focus {
+            background-color: lighten(#d32f2f, 40);
+        }
+    }
+
+    &.shadow-default {
+        box-shadow: none;
+    }
+
+    &:hover,
+    &:focus {
+        background: $background-blue-2;
+    }
+}
+
+.shadow-disabled {
+    box-shadow: none !important;
+}
+.shadow-default {
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+
+    &:hover,
+    &:focus {
+        box-shadow: 0px 5px 7px rgba(51, 51, 51, 0.2);
+    }
+}
+
+.size-sm {
+    padding: 6px 12px;
+}
+.size-md {
+    padding: 8px 20px;
+}
+.size-lg {
+    padding: 11px 22px;
+}
+
+.disabled {
+    cursor: not-allowed;
+
+    &:hover,
+    &:focus {
+        transform: translateY(0px);
+        box-shadow: none;
+    }
+}
+</style>
---
+++ vue-devchallenges-main/src/components/Input.vue
@@ -0,0 +1,182 @@
+<template>
+    <div class="input-component" :class="{
+            'variant-default': !error && !disabled,
+            'variant-error': error,
+            'disabled': disabled,
+            'size-sm': size == 'sm',
+            'size-md': size == 'md' || size == '' || size == null,
+            'full-width': fullWidth,
+        }">
+
+        <input :id="id" type="text" 
+        v-if="!rows"
+        v-bind="$attrs"
+        :value="value"
+        :class="{
+            'has-start-icon': startIcon,
+            'has-end-icon': endIcon,
+        }"
+        :disabled="disabled">
+
+        <textarea 
+        v-else
+        v-bind="$attrs"
+        :value="value"
+        :class="{
+            'has-start-icon': startIcon,
+            'has-end-icon': endIcon
+        }"
+        :disabled="disabled"
+        :rows="rows"></textarea>
+
+        <label :for="id">{{ label }}</label>
+        <span v-if="helperText" class="helper-text">{{ helperText }}</span>
+        <i v-if="startIcon" class="material-icons start-icon" :class="{'has-helper-text': helperText}">{{ startIcon }}</i>
+        <i v-if="endIcon" class="material-icons end-icon"  :class="{'has-helper-text': helperText}"> {{ endIcon }}</i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Input",
+    props: {
+        id: String,
+        label: { type: String, required: true },
+        fieldType: String,
+        error: Boolean,
+        disabled: Boolean,
+        helperText: String,
+        startIcon: String,
+        endIcon: String,
+        value: String,
+        size: String,
+        fullWidth: Boolean,
+        rows: Number
+    },
+    variant: String
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$gray-1: #333333;
+$gray-3: #828282;
+$gray-5: #E0E0E0;
+$gray-6: #F2F2F2;
+$blue-1: #2962FF;
+$red-1: #D32F2F;
+
+.input-component { 
+    display: inline-flex;
+    // flex-direction: column;
+    flex-direction: column-reverse;
+    position: relative;
+
+    label {
+        display: block;
+        font-size: 12px;
+        margin-bottom: 4px;
+    }
+
+    textarea {
+        resize: none;
+    }
+
+    input, textarea {
+        outline: 0;
+        border: 1px solid $gray-3;
+        box-sizing: border-box;
+        border-radius: 8px;
+
+        padding: 18px 12px;
+        font-size: 14px;
+
+        color: $gray-3;
+
+        &.has-start-icon { padding-left: 48px !important;  }
+
+        &.has-end-icon { padding-right: 48px !important; }
+    }
+
+    &.variant-default {
+        input, textarea {
+            &:hover { border: 1px solid $gray-1; }
+
+            &:focus { 
+                border: 1px solid $blue-1;
+
+                + label { color: $blue-1; }
+            }
+        }
+    }
+
+    &.variant-error {
+        label { color: $red-1; }
+
+        input, textarea {
+            border: 1px solid $red-1;
+
+            &:hover { 
+                border: 1px solid $gray-1;
+
+                + label { color: $gray-1; }
+                ~ .helper-text { color: $gray-1; }
+            }
+
+            &:focus { 
+                border: 1px solid $red-1;
+
+                + label { color: $red-1; }
+                ~ .helper-text { color: $red-1; }
+            }
+        }
+
+        .helper-text { color: $red-1 }
+    }
+
+    &.disabled {
+
+        input, textarea { 
+            border: 1px solid $gray-5;
+            cursor: not-allowed;
+            background: $gray-6;
+        }
+    }
+
+    .helper-text {
+        margin: 4px 5px;
+        color: $gray-3;
+        font-size: 10px;
+        line-height: 14px;
+
+        order: -1;
+    }
+
+    .material-icons {
+        position: absolute;
+        color: $gray-3;
+        
+        &.start-icon {
+            bottom: calc(50% - 24px);
+            left: 15px;
+        }
+
+        &.end-icon {
+            bottom: calc(50% - 24px);
+            right: 15px;
+        }
+
+        &.has-helper-text.end-icon, &.has-helper-text.start-icon   {
+            bottom: calc(62% - 24px);
+        }
+    }
+
+    &.size-sm { 
+        input, textarea { padding: 10px 12px; }
+    }
+    &.size-md { 
+        input, textarea { padding: 12px 18px; }
+    }
+    &.full-width { display: flex; }
+}
+</style>
